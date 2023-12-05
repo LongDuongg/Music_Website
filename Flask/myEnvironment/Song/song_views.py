934fd30e27ae6a2ad367f555e4db8c192ffe2d7d
Long Duong <long66704@gmail.com>
2023-12-05 22:37:01 +0700

    remove unneccesary stuffs

--- Flask/myEnvironment/Song/song_views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-song_views = Blueprint("song_views", __name__)
-
-@song_views.route("/")
-def song() :
-  return "<h1>song</h1>"
--- Flask/myEnvironment/User/user_views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
-
-user_views = Blueprint("user_views", __name__)
-
-@user_views.route('/')
-def user():
-  return "<h1>User</h1>"

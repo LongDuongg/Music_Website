283aea058c0ff14919e81c7dbacd61b38b60f679
Long Duong <long66704@gmail.com>
2023-12-14 23:59:49 +0700

    fix silly stuff

--- Flask/myEnvironment/Model/models.py
+++ Flask/myEnvironment/Model/models.py
@@ -6,7 +6,7 @@ class Playlist(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(100), nullable=False)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-  song = db.relationship('Song')
+  songs = db.relationship('Song')
 
   def __init__(self, title, user) :
     self.title = title

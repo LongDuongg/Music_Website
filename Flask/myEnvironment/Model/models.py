1e0985de0071fc58512be3945e6ac021cd77409a
Long Duong <long66704@gmail.com>
2023-12-14 23:12:28 +0700

    add play list class to model

--- Flask/myEnvironment/Model/models.py
+++ Flask/myEnvironment/Model/models.py
@@ -1,11 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+
+class Playlist(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+  def __init__(self, title, user_id) :
+    self.title = title
+    self.user_id = user_id
+
+
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(50), unique=True, nullable=False)
   password = db.Column(db.String(255), nullable=False)
   email = db.Column(db.String(100), unique=True, nullable=False)
+  playLists = db.relationship('Playlist')
   
   def __init__(self, username, password, email) :
     self.username = username
@@ -27,3 +39,6 @@ class Song(db.Model):
     self.duration = duration
     self.release_date = release_date
     self.genre = genre
+    
+    
+    

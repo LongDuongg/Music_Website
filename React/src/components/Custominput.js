5221c676c49dd875bd8456ae281fc0ad66bb9377
Long Duong <long66704@gmail.com>
2023-12-20 10:31:38 +0700

    add value and onchange props

--- React/src/components/Custominput.js
+++ React/src/components/Custominput.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 
 const Custominput = (props) => {
-    const{type, label, i_id, i_class} = props;
+    const{type, label, i_id, i_class, value, onChange} = props;
   return (
-    <div class="form-floating mb-3">
-      <input type={type} class={'form-control ${i_class}'} id={i_id} placeholder={label}/>
+    <div className="form-floating mb-3">
+      <input 
+        type={type} 
+        className={'form-control ${i_class}'} 
+        id={i_id} 
+        placeholder={label}
+        value={value}
+        onChange={(e) => onChange(e)}
+      />
       <label htmlFor={label}>{label}</label>
       
 </div>

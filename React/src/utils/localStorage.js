98985cf648e205261addc23dd4c2793497be4d95
Long Duong <long66704@gmail.com>
2023-12-22 11:04:59 +0700

    localStorage function

---
+++ React/src/utils/localStorage.js
@@ -0,0 +1,16 @@
+export const localStorage = (key) => {
+  return {
+    get: () => {
+      const item = localStorage.getItem(key);
+      return JSON.parse(item);
+    },
+
+    set: (value) => {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+
+    remove: () => {
+      localStorage.removeItem(key);
+    }
+  };
+};
